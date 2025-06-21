f7fd90c57702b16822cc88395bfc6fc5592ce3df
Abhisek <abhisek20188@gmail.com>
2025-06-21 19:18:45 +0545

    fix for clear blade temp erorr

--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -4,7 +4,11 @@
     </div>
     <h1 class="text-4xl font-bold text-red-600">create post bro</h1>
         <form action="{{route('post.store')}}" method="Post">
-
+            <div>
+                <x-input-label for="title" :value="__('Title')" />
+                <x-text-input id="title" class="block mt-1 w-full" type="text" name="title" :value="old('title')" required autofocus placeholder="Enter title" maxLength="75"/>
+                <x-input-error :messages="$errors->get('title')" class="mt-2" />
+            </div>
         </form>
     <div class="mt-8 text-gray-900">
     </div>
