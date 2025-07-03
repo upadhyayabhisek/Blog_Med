37802d311e0af3c57c3d3d0f68dcb06ad63f044c
Abhisek <g38099933@gmail.com>
2025-07-03 16:10:47 +0545

    Profile Service added

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ProfileUpdateRequest;
+use App\Services\PostService;
+use App\Services\ProfileService;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -14,6 +16,14 @@ class ProfileController extends Controller
     /**
      * Display the user's profile form.
      */
+
+    protected ProfileService $profileService;
+    public function __construct(ProfileService $profileService)
+    {
+        $this->profileService = $profileService;
+    }
+
+
     public function edit(Request $request): View
     {
         return view('profile.edit', [
